49865523eb219a0f0ea45d9fb792638dd4f5e5d2
Agustin3100 <5718@holbertonstudents.com>
2023-04-10 19:14:43 -0300

    Gather data from specific user and show only his completed tasks

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests, json
+from sys import argv
+"""Gather data from an API."""
+
+if len(argv) == 2:
+    employee_id = argv[1]
+
+    full_url = "https://jsonplaceholder.typicode.com/"
+    employee_info = full_url + "users/{}".format(employee_id)
+    todo_url = full_url + "todos?userId={}".format(employee_id)
+
+    employee_get = requests.get(employee_info)
+    todo_get = requests.get(todo_url)
+   # employee_response.raise_for_status()
+    # todo_response.raise_for_status()
+
+    employee_data = employee_get.json()
+    todo_data = todo_get.json()
+
+    employee_name = employee_data["name"]
+    num_total_tasks = len(todo_data)
+    num_done_tasks = sum(task["completed"] for task in todo_data)
+    completed_tasks = [task["title"] for task in todo_data if task["completed"]]
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_done_tasks, num_total_tasks))
+    for task_title in completed_tasks:
+        print("\t {}".format(task_title))
